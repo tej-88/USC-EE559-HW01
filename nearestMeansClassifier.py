72649d9de91ece7c12052552637ded2ef408fe3f
Tejas <tejas8acharya@gmail.com>
2023-06-14 22:00:20 +0530

    Add fit method for NearestMeansClassifier

--- nearestMeansClassifier.py
+++ nearestMeansClassifier.py
@@ -12,3 +12,24 @@ class NearestMeansClassifier():
         self.means = None
         self.classes = None
         self.features_idx = None
+
+    
+    def fit(self, X, y, features_idx):
+        self.classes = np.unique(y)
+        self.C = len(self.classes)
+        self.features_idx = features_idx
+        D = len(features_idx)
+
+        self.means = np.empty((self.C, D))
+
+        total_sum = np.zeros((self.C, D))
+        N = np.zeros((self.C, ))
+
+        for i in range(len(y)):
+            total_sum[y[i] - 1, :] += X[i, features_idx]
+            N[y[i] - 1] += 1
+        
+        for j in range(self.C):
+            self.means[j, :] = total_sum[j,:] / N[j]
+        
+        return
