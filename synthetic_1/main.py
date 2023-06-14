15f76bb1db3662838d2ffc3de0fd4056dfff5dff
Tejas <tejas8acharya@gmail.com>
2023-06-14 22:07:48 +0530

    Add completed synthetic_1/main.py

--- synthetic_1/main.py
+++ synthetic_1/main.py
@@ -21,4 +21,35 @@ def load_data(filename):
     X = data[:, :-1]
     y = data[:, -1].astype('int32')
     return (X, y)
-    
+
+
+#LOAD DATA
+X_train, y_train = load_data(TRAIN_DATASET_FILENAME)
+X_test, y_test = load_data(TEST_DATASET_FILENAME)
+
+features_idx = np.array([0, 1])
+
+#NEAREST MEANS CLASSIFIER MODEL
+model = NearestMeansClassifier()
+
+#Fit the Model
+model.fit(X_train, y_train, features_idx)
+
+#Predict the TRAIN DATA
+y_train_predict = model.predict(X_train)
+
+#Error Rate on TRAIN DATA
+train_error_rate = model.get_error_rate(y_train, y_train_predict)
+
+#Plot the Boundary
+model.plot_boundary(X_train, y_train)
+model.plot_boundary(X_train, y_train_predict)
+
+#Predict the TEST DATA
+y_test_predict = model.predict(X_test)
+
+#Error Rate on TEST DATA
+test_error_rate = model.get_error_rate(y_test, y_test_predict)
+
+print(f'Train Error Rate: {train_error_rate:.2f}%')
+print(f'Test Error Rate: {test_error_rate:.2f}%')
